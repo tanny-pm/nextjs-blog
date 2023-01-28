8015ebcebd7268ed72f49a2a90e97d0a8451cc44
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-01-28 23:10:55 +0900

    Add article pages

---
+++ components/date.js
@@ -0,0 +1,10 @@
+import { format, parseISO } from "date-fns";
+
+import React from "react";
+
+function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+}
+
+export default Date;
